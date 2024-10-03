4c43eac1fa651cd7ab9d19e3c0d19e223f56dd25
anewsdev <anewsdev@gmail.com>
2024-10-03 19:37:44 +0900

    init commit

---
+++ src/main/java/com/sparta/nbcpersnolprojectschedule/NbcPersnolProjectScheduleApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.nbcpersnolprojectschedule;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NbcPersnolProjectScheduleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NbcPersnolProjectScheduleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sparta/nbcpersnolprojectschedule/NbcPersnolProjectScheduleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.nbcpersnolprojectschedule;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NbcPersnolProjectScheduleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
